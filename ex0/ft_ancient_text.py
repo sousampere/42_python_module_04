98be24d7aea61b61d9fd80cf1a85822f8afb7493
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-01-06 13:16:03 +0100

    add ex0 DONE

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,17 @@
+#!/bin/bash/python3
+
+
+file_name = "ancient_fragment.txt"
+print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+print(f"Accessing Storage Vault: {file_name}")
+
+try:
+    with open(file_name, 'r') as file:
+        print("Connection established...\n")
+        content = file.read()
+        print("RECOVERED DATA:")
+        print(content)
+        print("\n\033[0;32m🟩 Data recovery complete."
+              "Storage unit disconnected.\033[0;37m")
+except Exception:
+    print("\033[0;31m🟥 ERROR: Storage vault not found.\033[0;37m")
